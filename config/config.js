3daa06f528391bc62ca55d85fea2c1a400958c14
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-06-03 10:47:26 +0200

    Sección 12 terminada - backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '192714937071-1oh5rotihdm5q13n448bdaji9a5pp8u3.apps.googleusercontent.com';
+module.exports.SECRET = 'oqcKFVK0DfKMirHvBfbdr5s2';
